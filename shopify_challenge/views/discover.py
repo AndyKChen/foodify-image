75c2b72eaf5c452263b361b619cf9abc48d53f02
Andy Chen <andychen2325@gmail.com>
2021-05-07 23:44:26 -0400

    setup s3 bucket and cloudfront to retrieve images

--- shopify_challenge/views/discover.py
+++ shopify_challenge/views/discover.py
@@ -7,7 +7,15 @@ from flask.views import MethodView
 
 class Discover(MethodView):
     def get(self):
-        ACCESS_KEY_ID = os.environ.get('ACCESS_KEY_ID')
-        SECRET_ACCESS_KEY = os.environ.get('SECRET_ACCESS_KEY')
-        S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-        return render_template('discover.html')
+
+        cloudfront = os.environ.get('CLOUDFRONT_DOMAIN')
+
+        s3_resource = boto3.resource(
+            's3',
+            aws_access_key_id = os.environ.get('ACCESS_KEY_ID'),
+            aws_secret_access_key = os.environ.get('SECRET_ACCESS_KEY')
+        )
+        my_bucket = s3_resource.Bucket(os.environ.get('S3_BUCKET_NAME'))
+        summaries = my_bucket.objects.all()
+
+        return render_template('discover.html', my_bucket=my_bucket, files=summaries, cloudfront=cloudfront)
