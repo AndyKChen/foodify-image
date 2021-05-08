971f94f2055c266e32a0a17b3f102db905f9f71d
Andy Chen <andychen2325@gmail.com>
2021-05-08 16:28:51 -0400

    add error handling for no image selected

--- shopify_challenge/views/upload.py
+++ shopify_challenge/views/upload.py
@@ -5,7 +5,7 @@ import time
 import requests
 from multiprocessing.pool import ThreadPool
 
-from flask import redirect, render_template, session, request
+from flask import redirect, render_template, session, request, flash
 from flask.views import MethodView
 
 from shopify_challenge.helpers.decorators import login_required
@@ -40,10 +40,17 @@ class Upload(MethodView):
     @login_required
     def post(self):
         uploaded_files = request.files.getlist("file")
+        access_type = request.form.get("access_type")
+        print(access_type)
         presigned_posts = []
+
         for file in uploaded_files:
+            if (file.filename == ""):
+                flash("You must select at least one image!", 'danger')
+                return render_template("upload.html")
             new_post = create_presigned_post(os.environ.get('S3_BUCKET_NAME'), str(uuid.uuid4()))
             presigned_posts.append(new_post)
+
         pool = ThreadPool(processes=20)
         start = time.time()
         pool.starmap(upload, zip(presigned_posts, uploaded_files))
