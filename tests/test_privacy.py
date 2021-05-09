f5e7e3ec3640dadd03bbaa2279e2d18c89099523
Andy Chen <andychen2325@gmail.com>
2021-05-09 16:44:29 -0400

    add test for changing image privacy

---
+++ tests/test_privacy.py
@@ -0,0 +1,19 @@
+from .helpers import register_test_user, login_test_user, upload_single_image, edit_image
+from shopify_challenge.models.image import ImageModel
+
+def test_change_image_privacy(client):
+    register_test_user(client)
+    login_test_user(client)
+    upload_single_image(client)
+
+    private_images = ImageModel.get_private_images_by_username('test_user')
+    identifier = private_images[0].identifier
+    assert private_images[0].private == True
+
+    res = edit_image(client, identifier, 'make public')
+    image = ImageModel.get_image_by_identifier(identifier)
+    assert image.private == False
+    assert res.status_code == 200
+
+    # delete image from s3 and db
+    edit_image(client, identifier, 'delete')
